5302471efc326ce26aec97a2e983d3ff38a65326
Dmitry Biryukov <dim.bir2017@yandex.ru>
2024-01-29 20:46:09 +0300

    created model question

--- src/app/models.py
+++ src/app/models.py
@@ -1,17 +1,18 @@
+from datetime import datetime
+
 from sqlalchemy import TIMESTAMP, Column, Integer, String
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
 Base: DeclarativeMeta = declarative_base()
 
 
-class Level(Base):
-    __tablename__ = 'level'
+class Question(Base):
+    __tablename__ = 'question'
     id = Column(Integer, primary_key=True)
-    winter = Column(String(2))
-    summer = Column(String(2))
-    autumn = Column(String(2))
-    spring = Column(String(2))
-
+    added_at = Column(TIMESTAMP, default=datetime.utcnow)
+    question = Column(String)
+    answer = Column(String)
+    additional_material = Column(String)
 
 
 
